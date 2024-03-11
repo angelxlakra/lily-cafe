3b1df5f923ad04a8cc9fb27813a18fa151377bdd
Angel Lakra <angel.lakra@chainlabs.in>
2024-03-11 10:10:51 +0530

    added comments for understanding code

--- packages/server/graphql/resolvers/addOrder.mutation.js
+++ packages/server/graphql/resolvers/addOrder.mutation.js
@@ -18,11 +18,12 @@ const addOrder = async (parent, args) => {
       return newItem;
     })
   );
-  const today = moment().format("YYYY-MM-DD");
+  const today = moment().format("YYYY-MM-DD"); // time for today at 12:00 AM
   const utcOffset = moment().utcOffset();
-  console.log({utcOffset});
-  const utcToday = moment(today).add(-utcOffset, 'minutes')
+  console.log({utcOffset});           // 330 minutes ~ +5:30 hours
+  const utcToday = moment(today).add(-utcOffset, 'minutes') // subtract +5:30 hours to get UTC time of today
   console.log({
+    // Yahaan pe format sirf time dekhne ke lie kiya hua hai 
     now: moment().add(-utcOffset, 'minutes').format("YYYY-MM-DD, LTS"),
     gte: utcToday.format("YYYY-MM-DD, LTS"),
     lt: moment(utcToday).add(1, "d").format("YYYY-MM-DD, LTS"),
@@ -30,8 +31,9 @@ const addOrder = async (parent, args) => {
   const todayOrdersCount = await prisma.order.count({
     where: {
       createdAt: {
-        gte: new Date(utcToday),
-        lt: new Date(moment(utcToday).add(1, "d")),
+        // createdAt is in utc time so we converted both below times to utc
+        gte: new Date(utcToday), // greater than equal to utc time of today
+        lt: new Date(moment(utcToday).add(1, "d")), // less than utc time of next day
       },
     },
   });
@@ -43,6 +45,9 @@ const addOrder = async (parent, args) => {
       orderNo: todayOrdersCount + 1,
     },
   });
+
+  // after creating order, we will decrease stock by quantity when order is confirmed
+
   return response;
 };
 
